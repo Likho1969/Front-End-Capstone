3037956d17c970c3dd5b2f865f561b92b7d085c6
admin <you@example.com>
2021-09-09 13:52:19 +0200

    changes

--- js/login.js
+++ js/login.js
@@ -38,22 +38,22 @@ function login() {
         storage.setItem("jwt-token", data["access_token"]);
         storage.setItem("username", username);
         storage.setItem("password", password);
-        window.location.href = "/index.html";
+        window.location.href = "/home.html";
       }
     });
 }
 
-document.querySelector('.login').addEventListener('submit', (e) => {
+document.querySelector('.loginform').addEventListener('submit', (e) => {
   e.preventDefault()
   login()
 })
 
-function subscribe() {
-  let name = document.querySelector('.name').value
-  let surname = document.querySelector('.surname').value
-  let email = document.querySelector('.email').value
-  let username = document.querySelector('.username').value
-  let password = document.querySelector('.password').value
+function regitser() {
+  let name = document.querySelector('.namebox1').value
+  let surname = document.querySelector('.namebox2').value
+  let email = document.querySelector('.mobilepw').value
+  let username = document.querySelector('.namebox3').value
+  let password = document.querySelector('.mobilepw2').value
   fetch("https://dynamicoakfx.herokuapp.com/registration/", {
     method: "POST",
     body: JSON.stringify({
@@ -78,19 +78,19 @@ function subscribe() {
   });
 }
 
-document.querySelector('.subscribe').addEventListener('submit', (e) => {
+document.querySelector('.signupform').addEventListener('submit', (e) => {
   e.preventDefault()
-  subscribe()
+  regitser()
 })
 
-function loginToggle() {
-  let login = document.querySelector('#login')
-  let regitser = document.querySelector('#subscribe')
-  login.classList.toggle("active");
-  regitser.classList.toggle("active");
-  if (regitser.classList.contains("active")) {
-    login.classList.add("hide");
-  } else {
-    regitser.classList.add("hide");
-  }
-}
+// function loginToggle() {
+  // let login = document.querySelector('#login')
+  // let regitser = document.querySelector('#subscribe')
+  // login.classList.toggle("active");
+  // regitser.classList.toggle("active");
+  // if (regitser.classList.contains("active")) {
+    // login.classList.add("hide");
+  // } else {
+    // regitser.classList.add("hide");
+  // }
+// }
